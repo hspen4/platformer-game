1156b718f7fe3bd3e26ab8bb1281dbd866b8a498
hspen4 <a1801285@student.adelaide.edu.au>
2020-10-14 02:33:22 +1030

    boundary checking fixes

--- Floor.cpp
+++ Floor.cpp
@@ -9,7 +9,7 @@ Floor::Floor(int left_x, int right_x, int y)
 
 void Floor::collide(PhysicsObject *obj) {
     if (obj->get_y() == y) {
-        if (obj->get_x() > left_x && obj->get_x() < right_x) {
+        if (obj->get_x() >= left_x && obj->get_x() <= right_x) {
             // the object's inside the floor, push it upwards
             int new_y = obj->get_y() - 1;
             obj->set_y(new_y);
@@ -20,7 +20,7 @@ void Floor::collide(PhysicsObject *obj) {
 
 void Floor::check_grounded(PhysicsObject *obj) {
     if (obj->get_y() == (y - 1) && obj->get_momentum_y() == 0) {
-            if (obj->get_x() > left_x && obj->get_x() < right_x) {
+            if (obj->get_x() >= left_x && obj->get_x() <= right_x) {
                 // object is grounded, set status
                 obj->set_grounded(true);
             }
