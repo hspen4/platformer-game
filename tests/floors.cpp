18d9fa97c533e70665ae40c50dabe93dbf64e7de
b <frumgerth@gmail.com>
2020-10-14 11:22:10 +1030

    fix tests

--- tests/floors.cpp
+++ tests/floors.cpp
@@ -9,35 +9,42 @@ void floors() {
 
 
     // after one tick, there should be a 1 tile gap between p and the floor
-    for (int i = 0; i <= p.get_tick_limit(); i++) p.tick();
-    f.collide(&p);
+    for (int i = 0; i <= p.y_tick.frequency; i++) {
+        p.tick();
+        f.collide(&p);
+    }
     assertm(p.get_y() == 3, "first fall tick");
     assertm(p.grounded() == false, "first fall tick");
 
     // after the second tick, p should have hit the floor
-    for (int i = 0; i <= p.get_tick_limit(); i++) p.tick();
-    f.collide(&p);
+    for (int i = 0; i <= p.y_tick.frequency; i++) {
+        p.tick();
+        f.collide(&p);
+    }
     assertm(p.get_y() == 4, "second fall tick");
 
     // after the third, p should still be on the floor
-    for (int i = 0; i <= p.get_tick_limit(); i++) p.tick();
-    f.collide(&p);
-    assertm(p.get_y() == 4, "third fall tick");
+    for (int i = 0; i <= p.y_tick.frequency; i++) {
+        p.tick();
+        f.collide(&p);
+    }
     assertm(p.grounded() == true, "third fall tick");
+    assertm(p.get_y() == 4, "third fall tick");
 
     // move p inside the floor (this happens if you jump from below)
     // the floor should push p upwards
     p.set_y(5);
-    for (int i = 0; i <= p.get_tick_limit(); i++) p.tick();
     f.collide(&p);
     assertm(p.get_y() == 4, "pushed from inside floor");
 
     // move p to the right, they should start falling
-    p.set_momentum_x(2);
+    p.set_x(15);
     for (int i = 0; i < 3; i++) {
-        for (int i = 0; i <= p.get_tick_limit(); i++) p.tick();
+        for (int i = 0; i <= p.y_tick.frequency; i++) {
+            p.tick();
+            f.collide(&p);
+        }
         //std::cout << p.get_x() << " " << p.get_y() << " " << p.grounded() << " " << p.get_momentum_y() << std::endl;
-        f.collide(&p);
     }
     assertm(p.get_y() > 4, "falling after walking off a platform");
     assertm(p.grounded() == false, "falling after walking off a platform");
