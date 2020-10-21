8e84e70f23eac345b445460f78dcb9f58e373c9c
B <kittceon@gmail.com>
2020-10-21 13:24:25 +1030

    more tests

--- tests/physics.cpp
+++ tests/physics.cpp
@@ -18,6 +18,10 @@ void physics(Harness *assert) {
     assert->gt(a.get_x(),  last_x, "momentum");
 
     double last_dx = a.get_dx();
+    a.set_grounded(false);
+    for (int i = 0; i < 120; i++) a.tick();
+    assert->eq(a.get_dx(), last_dx, "no friction in air");
+
     a.set_grounded(true);
     for (int i = 0; i < 120; i++) a.tick();
     assert->lt(a.get_dx(), last_dx, "friction");
